84ca492cc857998a30755f355792f42b4b31dbc5
John Doe <jdoe@example>
2016-01-28 20:18:31 +0000

    Initial Apache Cookbook

---
+++ recipes/server.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: apache
+# Recipe:: server
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+package 'apache2'
+
+file '/var/www/html/index.html' do
+ content '<h1>Hello, World!</h1>'
+end 
+
+service  'apache2' do 
+  action [:enable ,:start]
+end 
